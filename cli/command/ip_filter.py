16981d37b9d481b537a5930e210335b2017f0038
bogay <pojay11523@gmail.com>
2021-10-25 01:11:50 +0800

    feat(command.ip_filter): add a command to add ip filter

--- cli/command/ip_filter.py
+++ cli/command/ip_filter.py
@@ -1,16 +1,56 @@
 import json
-import click
+import logging
+from typing import Optional
 from cli.core import (
     Homework,
     Config,
 )
 from cli.core.auth import logined_session
-from typing import Optional
+import click
 
 
 @click.group()
 def ip_filter():
     pass
+
+
+@ip_filter.command()
+@click.option('-i', '--id', help='Homework ID')
+@click.option('-n', '--name', help='Homework name')
+@click.option(
+    '-c',
+    '--course',
+    help='Course name, should be provided with -n/--name option',
+    required=True,
+)
+@click.argument('ip')
+def add(
+    ip: str,
+    course: str,
+    id: Optional[str],
+    name: Optional[str],
+):
+    '''
+    Add IP filter to homework
+    '''
+    if id is not None:
+        hw = Homework.get_by_id(id)
+    elif name is not None:
+        hw = Homework.get_by_name(course, name)
+    else:
+        raise ValueError('Option error, either id or name should be provided.')
+    with logined_session() as sess:
+        url = f'{Config.API_BASE}/homework/{course}/{hw.name}/ip-filters'
+        resp = sess.patch(
+            url,
+            json={'patches': [{
+                'op': 'add',
+                'value': ip,
+            }]},
+        )
+        logging.debug(resp.text)
+        if resp.status_code != 200:
+            exit(1)
 
 
 @ip_filter.command()
